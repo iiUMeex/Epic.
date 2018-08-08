3d597a5c77a16ef671d5490a97016d20b87a2154
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2018-08-08 06:28:21 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -413,7 +413,6 @@ client.on('message', msg => {
   //By EpicKinG.
 
 const fs = require('fs');
-const db = fs.database();
 client.oN("message", message => {
         if (message.content === "setprefix") {
         if (message.author.id !== "274923685985386496" && !message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`U don't have permission to do that`);
