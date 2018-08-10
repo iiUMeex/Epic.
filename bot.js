85b35a622dab15b578a16f39991617c0f0ef67de
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2018-08-10 06:47:16 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -463,7 +463,8 @@ client.on("message", message => {
 
   const fs = require('fs');
 const db = fs.database();
-client.oN("message", message => {
+
+client.on("message", message => {
         if (message.content === "setprefix") {
         if (message.author.id !== "274923685985386496" && !message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`U don't have permission to do that`);
   let args = message.content.split(" ").slice(1);
