964f0f45e071a6b240894832c31043baaf1a9809
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2018-08-11 13:29:01 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7687,30 +7687,29 @@ if (message.content.startsWith(prefix + `Lmgtfy`)) {
         else message.channel.send(`"${textQuery}"\n**<${url}>**`);
 }}});
 
+var prefix = "-";
+
 const math = require('math-expression-evaluator');
 const stripIndents = require('common-tags').stripIndents;
 
 client.on('message', msg => {
-if (msg.content.startsWith(prefix + 'cal')) {
-  let args = msg.content.split(" ").slice(1);
-      const question = args.join(' ');
-  if (args.length < 1) {
-      msg.reply('**من فضلك .. قم بكتابة سؤال **.');
+ if (msg.content.startsWith(prefix + 'cal')) {
+    let args = msg.content.split(" ").slice(1);
+        const question = args.join(' ');
+    if (args.length < 1) {
+        msg.reply('Specify a equation, please.');
 } else {    let answer;
-  try {
-      answer = math.eval(question);
-  } catch (err) {
-      return msg.reply(`Error: ${err}`);
-  }
+    try {
+        answer = math.eval(question);
+    } catch (err) {
+        msg.reply(`Error: ${err}`);
+    }
 
-  const embed = new Discord.RichEmbed()
-  .setThumbnail('https://banner2.kisspng.com/20180215/ade/kisspng-office-supplies-animation-calculator-5a85e764e3aa68.4914103215187249649325.jpg')
-.setDescription(`**
- السؤال يقولك :thinking:  : ${question}
-
- طبعا الاجابة :writing_hand: : ${answer}**
-`)
-  msg.channel.send(embed)
-  }
+    const embed = new Discord.RichEmbed()
+    .addField("**السؤال**: ",`**${question}**`, true)
+    .addField("**الناتج**: ",`**${answer}**`, true)
+    .setFooter("Epic. حاسبه")
+    msg.channel.send(embed)
+    }
 };
 });
