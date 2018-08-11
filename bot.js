f6d6967e89789c92afe1077111bd7308e16684fb
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2018-08-11 07:26:52 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7582,3 +7582,46 @@ message.member.removeRole(message.guild.roles.find("name", "100"));
   }
 	
 });
+
+client.on('message', message => {
+  if(message.content.startsWith(prefix + "saraha")) {
+    let mention = message.mentions.users.first();
+    let args = message.content.split(' ').slice(2).join(' ');
+    let filter = s => s.author.id === message.author.id;
+
+    var msg;
+
+    if(!mention) return message.channel.send(':x:| **منشن الشخص المطلوب ارسال الرسالة له**');
+    if(mention === client.user) return message.channel.send(':x:| **لا يمكنني ارسال رسالة لنفسي**');
+    if(mention.bot) return message.channel.send(':x:| **لا يمكنك ارسال الرسالة لبوت**');
+    if(mention === message.author) return message.channel.send(':x:| **لا يمكنك مصارحة نفسك**').then(m => { m.delete(3500)});
+    if(mention && !args) {
+      message.delete();
+      message.channel.send(`⚒| **\`عملية اعادة البناء\` انت لم تقم بكتابة الرسالة  اكتبها الان .. امامك 30 ثانية**`).then(m => {
+        message.channel.awaitMessages(filter, {
+          max: 1,
+          time: 30000,
+          errors: ['time']
+        }).then(collected => {
+          msg = collected.first().content;
+          collected.first().delete();
+          try {
+            m.edit(':white_check_mark:| **تم ارسال الصراحة**');
+            mention.send(`${mention}, لقد تم مصارحتك\n» الرسالة : ${msg}\n» التاريخ : ${moment().format('MMM Do YY')}`);
+          } catch(e) {
+            message.channel.send(':x:| **لم استطع ارسال الرسالة**');
+          }
+        });
+      });
+    } else if(mention && args) {
+      message.delete();
+      try {
+        message.channel.send(':white_check_mark:| **تم ارسال الصراحة**');
+        mention.send(`${mention}, لقد تم مصارحتك\n» الرسالة : ${args}\n» التاريخ : ${moment().format('MMM Do YY')}`);
+      } catch(e) {
+        console.log(e);
+        message.channel.send(':x:| **لم استطع ارسال الرسالة**');
+      }
+    }
+  }
+});
