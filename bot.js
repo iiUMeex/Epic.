d7c71c1686f7adcd942be1940b073996879c8d20
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2018-08-19 13:09:50 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -393,6 +393,7 @@ General's Commands. :earth_asia:
 ــــــــــــــــــــــــــــــــــــــــــــــــــــــ
 اوامر الالعاب
 -cuttweet للعب لعبة كت تويت :regional_indicator_c: :regional_indicator_u: :regional_indicator_t: 
+-speed أسرع كتابه :speech_balloon: 
 ـــــــــــــــــــــــــــــــــــــــــــــــــــــــ
 اوامر الميوزك
 -play ⇏ لتشغيل أغنية برآبط أو بأسم :musical_note: 
@@ -2445,4 +2446,60 @@ client.on('message', message => {/// الفا كود | Emoko#0001
   .addField("**سيرفرات يلي البوت موجود فيها: **" , client.guilds.size)
   message.channel.sendEmbed(embed);
     }/// الفا كود | Emoko#0001
+});
+
+client.on('message', message => {
+if (!points[message.author.id]) points[message.author.id] = {
+    points: 0,
+  };
+  if(!message.guild) return;
+    let id = message.author.id,prefix="-";
+    if (spee[id] && (new Date).getTime() - spee[id] < 15*1000) {
+        let r = (new Date).getTime() - spee[id];
+        r = 15*1000 - r;
+    message.channel.send(`**Sorry, Please Wait ${pretty(r, {verbose:true})}...**`).then(m => m.delete(5000));
+    return;
+    }
+    if ( message.content == prefix+'speed'){
+       
+        try{
+}catch(e){
+ 
+}
+ 
+    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+ 
+ 
+const item = type[Math.floor(Math.random() * type.length)];
+const filter = response => {  
+    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+};
+message.channel.send('**Game is Start now...!**').then(msg => {
+ 
+ const embed = new Discord.RichEmbed()
+ .setColor("0054dd")
+     .setAuthor(`⏳ |You have »15« seconds to type the word`)
+          .setImage(`${item.type}`)
+ .setFooter(`${message.author.tag}`, message.author.avatarURL)
+ 
+ 
+         
+msg.channel.send(embed).then(() => {
+        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
+        .then((collected) => {
+                  const sh = new Discord.RichEmbed()
+  .setColor("04791c")
+  .setDescription('**✅ |Good Job +1P**')
+   .setFooter(`${collected.first().author}`)
+  message.channel.sendEmbed(sh);
+            let won = collected.first().author; // في هذا السطر يقوم الكود بسحب الأي دي الذي قام بالأجابة اولاً
+            points[won.id].points++;
+          })
+          .catch(collected => { // في حال لم يقم أحد بالإجابة
+            message.channel.send(`🔚 |**Time Is End**`);
+          })
+        })
+    })
+    spee[id] = (new Date).getTime()
+}
 });
