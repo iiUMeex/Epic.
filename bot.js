b93063d8a617be1561780c7d69a0dceb4414b126
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2018-08-23 05:39:29 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2523,31 +2523,3 @@ client.on('guildMemberAdd',async member => {
     });
   }
 });
-
-lient.on('message', message => {
-  if (!message.content.startsWith(PREFIX)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== "295216776428388362") return;
-
-if (message.content.startsWith(PREFIX + 'setgame')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`Playing: **${argresult}`)
-} 
-
-if (message.content.startsWith(PREFIX + 'setstream')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/v5bz");
-	 console.log('test' + argresult);
-    message.channel.sendMessage(`Streaming: **${argresult}`)
-} 
-
-if (message.content.startsWith(PREFIX + 'setname')) {
-  client.user.setUsername(argresult).then
-	  message.channel.sendMessage(`Username Changed To **${argresult}**`)
-  return message.reply("You Can change the username 2 times per hour");
-} 
-if (message.content.startsWith(PREFIX + 'setavatar')) {
-  client.user.setAvatar(argresult);
-   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
-}
-});
