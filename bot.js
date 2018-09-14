533963e9c1bc03146f547e4bd5e6915b73cbaf05
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2018-09-14 05:09:09 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2221,8 +2221,3 @@ client.on('message', message => {/// الفا كود | Emoko#0001
   message.channel.sendEmbed(embed);
     }/// الفا كود | Emoko#0001
 });
-
-client.on("guildMemberAdd", (member) => {
-let channel = member.guild.channels.find('name', 'welcome');
-channel.send('**Welcome** ' + `${member}` + ' **To** ' + `__${member.guild.name}__` + ' **Server** :two_hearts:')          
-channel.sendFile(canvas.toBuffer());
