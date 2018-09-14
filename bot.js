ec577cefe3761e763a0f1ed2ba950427e86eb3bb
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2018-09-14 11:52:48 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -278,7 +278,7 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 });
 
 client.on("message", message => {
- if (message.content === `${prefix}`) {
+ if (message.content === `-mhelp`) {
   const embed = new Discord.RichEmbed() //by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
       .setColor("#000000")//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
       .setDescription(`
