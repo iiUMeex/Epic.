09ae215988610080df20acd252ee1bf311b5bec7
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2018-09-14 05:20:14 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,23 +22,6 @@ client.on('message', msg => {
   //كود الرد التلقائي
   //By EpicKinG.
 
-  client.on("message", message => {
-    if(message.content.startsWith(prefix + "server")) {
-        if(!message.member.hasPermission("SEND_MESSAGES")) return message.channel.send("**ليس لديك البرمشن المطلوب لاستخدام هذا الامر ❌**");
-        const embed = new Discord.RichEmbed()
-        .setAuthor(message.guild.name, message.guild.iconURL)
-        .setColor("RANDOM")
-  
-  .addField('**عدد اعضاء السيرفر 👤 **' , `${message.guild.memberCount}`)
-  .addField('**اونر السيرفر 👑**' , `${message.guild.owner.user.username}`)
-  .addField(`**الرومات :scroll: **`,true)
-  .addField(`# الكتابية`, `${message.guild.channels.filter(m => m.type === 'text').size}`)
-  .addField( `:loud_sound: الصوتية`,`${message.guild.channels.filter(m => m.type === 'voice').size}`)
-  .addField(`**عدد الرتب**:briefcase:`,`${message.guild.roles.size}`)
-        message.channel.send({embed:embed})
-    }
-  });
-
   client.on('message', message => {
           
 
