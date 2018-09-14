7c4fa103e171b761cd82fe12739921122968c750
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2018-09-14 11:34:54 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,62 +19,6 @@ client.on('message', msg => {
     }
   });
 
-  client.on('guildMemberRemove', member => {
-      var embed = new Discord.RichEmbed()
-      .setAuthor(member.user.username, member.user.avatarURL)
-      .setThumbnail(member.user.avatarURL)
-      .setTitle(`الله معاك ✋:skin-tone-1: 😔`)
-      .setDescription(`مع السلامه تشرفنا بك ✋:skin-tone-1: 😔 `)
-      .addField('👤   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-      .setColor('RED')
-      .setFooter(`==== نــتــمــنــآ لــكــم آســتــمـــتــآع ====`, 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
-  
-  var channel =member.guild.channels.find('name', 'welcome')
-  if (!channel) return;
-  channel.send({embed : embed});
-  })
-
-client.on('message', message => {
-  if(!message.channel.guild) return;
-if(message.content.startsWith('-bc')) {
-if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-let copy = "Admin";
-let request = `Requested By ${message.author.username}`;
-if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-msg.react('✅')
-.then(() => msg.react('❌'))
-.then(() =>msg.react('✅'))
-
-let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-reaction1.on("collect", r => {
-message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
-message.guild.members.forEach(m => {
-var bc = new
-Discord.RichEmbed()
-.setColor('RANDOM')
-.setTitle('Broadcast')
-.addField('Server', message.guild.name)
-.addField('Sender', message.author.username)
-.addField('Message', args)
-.setThumbnail(message.author.avatarURL)
-.setFooter(copy, client.user.avatarURL);
-m.send({ embed: bc })
-msg.delete();
-})
-})
-reaction2.on("collect", r => {
-message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
-msg.delete();
-})
-})
-}
-});
-
       client.on('message', message => {
         if (true) {
       if (message.content === '-invite') {
