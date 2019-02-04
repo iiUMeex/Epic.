93153a27c29da381f4406e7826d2fde61f891844
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2019-02-04 19:50:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,248 +1,94 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-var prefix = "-"
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-client.login(process.env.BOT_TOKEN);  //لا تحط التوكن حقك هنا
-
 client.on('message', message => {
-  if (message.author.codes) return;
-  if (!message.content.startsWith(prefix)) return;
+if (message.content.startsWith(prefix + 'help')) { /// This is The DMS Code Send The Help In DMS // Code By NotGucci
+    let pages = [`
+***__وصف عن البوت__***
+**
+:gem:  البوت فيه كثير ميزات حلوة و جميلة
+ ا:rocket: البوت يعمل 24 ساعه 
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
+**
+        ***__General orders__***
+**
+?${prefix}server/يعرض لك معلومات عن السيرفر?
+?${prefix}bot/يعرض لك كل معلومات البوت?
+?${prefix}count/يعرض لك عدد الاشخاص بالسيرفر بدون بوتات?
+?${prefix}invite/ لدعوة البوت في سيرفرك ?
+?${prefix}rooms/يعرض لك كل الرومات الي بالسيرفر مع عددها?
+?${prefix}roles/يعرض لك كل الرانكات بالسيرفر بشكل جميل?
+?${prefix}emojilist/يعرض لك كل الايموجيات الي بالسيرفر?
+?${prefix}say/يكرر الكلام الي تكتبو?
+?${prefix}id/معلومات عنك?
+?${prefix}avatar/صورتك او صورة الي تمنشنو?
+?${prefix}embed/يكرر الي تقولو بشكل حلو? 
+?${prefix}support/سيرفر الدعم?
+**
+  `
+,`
+        ***__Administrative Orders__***
+**
+?${prefix}move @user /  لسحب الشخص الى روومك?  
+?${prefix}bc / رسالة جماعية الى كل اعضاء السيرفر?
+?${prefix}role @user <rank> / لأعطاء رتبة لعضو معين?
+?${prefix}hchannel / اخفاء الشات?
+?${prefix}schannel / اضهار الشات المخفية?
+?${prefix}clr <numbr> / مسح الشات بعدد?
+?${prefix}clear / مسح الشات?
+?${prefix}mute @user <time> / اعطاء العضو ميوت ?
+?${prefix}unmute @user / لفك الميوت عن الشخص ?
+?${prefix}kick @user <reason> / طرد الشخص من السيرفر?
+?${prefix}ban @user <reason> / حضر الشخص من السيرفر?
+?${prefix}mutechannel / تقفيل الشات?
+?${prefix}unmutechannel / فتح الشات?
+?${prefix}mvall/لأتحريك كل الاغضاء للروم الصوتيه?
+?${prefix}dc / مسح كل الرومات?
+?${prefix}dr / <مسح كل الرانكات <لازم تكون رانك البوت فوق كل الرانكات?
+?${prefix}setleave/لأنشاء روم مغادر العضوا?
+?${prefix}setmedia/لأنشاء رووم مخصص لصور و الخ?
+?${prefix}togglemedia/لتشغيل الروم و اطقائها?
+?${prefix}infomedia/معلومات عن روم الي انت أنشأتها?
+?${prefix}gstart/للأنشاء جيف اواي شرط لا تمنشن الروم اكتب اسمها فقط?
 
-  let args = message.content.split(" ").slice(1);
+**
 
-  if (command == "ban") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-         
-  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**انت لا تملك الصلاحيات المطلوبه**");
-  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-  
-  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-  if (!message.guild.member(user)
-  .bannable) return message.reply("**يجب ان تكون رتبة البوت اعلي من رتبه الشخص المراد تبنيدة**");
+`]
+    let page = 1;
+
+    let embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setFooter(`Page ${page} of ${pages.length}`)
+    .setDescription(pages[page-1])
+
+    message.author.sendEmbed(embed).then(msg => {
+
+        msg.react('?').then( r => {
+            msg.react('?')
 
 
-  message.guild.member(user).ban(7, user);
+        const backwardsFilter = (reaction, user) => reaction.emoji.name === '?' && user.id === message.author.id;
+        const forwardsFilter = (reaction, user) => reaction.emoji.name === '?' && user.id === message.author.id;
 
-message.channel.send(`**:white_check_mark: ${user.tag} banned from the server ! :airplane: **  `)
 
-}
-});
+        const backwards = msg.createReactionCollector(backwardsFilter, { time: 2000000});
+        const forwards = msg.createReactionCollector(forwardsFilter, { time: 2000000});
 
-client.on('message', message => {
- 
-    if(message.content.startsWith(prefix + 'rep')) {
-      if(!message.channel.guild) return;
-                    moment.locale('en');
-                  var getvalueof = message.mentions.users.first()
-                    if(!getvalueof) return message.channel.send(`**:mag: |  ${message.author.username}, the user could not be found.    **`);
-                       if(getvalueof.id == message.author.id) return message.channel.send(`**${message.author.username}, you cant give yourself a reputation !**`)
-    if(profile[message.author.id].reps != moment().format('L')) {
-            profile[message.author.id].reps = moment().format('L');
-            profile[getvalueof.id].rep = Math.floor(profile[getvalueof.id].rep+1);
-         message.channel.send(`** :up:  |  ${message.author.username} has given ${getvalueof} a reputation point!**`)
-        } else {
-         message.channel.send(`**:stopwatch: |  ${message.author.username}, you can raward more reputation  ${moment().endOf('day').fromNow()} **`)
-        }
-       }
-if (err) console.error(err);
-})
-});
- 
-client.on("message", (message) => {
-  let men = message.mentions.users.first()
- 
-  if (message.author.bot) return;
-    if (message.author.id === client.user.id) return;
-    if(!message.channel.guild) return;
-if (message.content.startsWith(prefix + 'credit')) {
-  if(men) {
-    if (!profile[men.id]) profile[men.id] = {
-    lastDaily:'Not Collected',
-    credits: 1,
-  };
-  }
-  if(men) {
-message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${profile[men.id].credits}$` + "`.**")
-} else {
-  message.channel.send(`** ${message.author.username}, your :credit_card: balance` + " is `" + `${profile[message.author.id].credits}$` + "`.**")
-}
-}
- 
-if(message.content.startsWith(prefix + "daily")) {
-  if(profile[message.author.id].lastDaily != moment().format('day')) {
-    profile[message.author.id].lastDaily = moment().format('day')
-    profile[message.author.id].credits += 200
-     message.channel.send(`**${message.author.username} you collect your \`200\` :dollar: daily pounds**`)
-} else {
-    message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
-}
-  }
 
- 
- let cont = message.content.slice(prefix.length).split(" ");
-let args = cont.slice(1);
-let sender = message.author
-if(message.content.startsWith(prefix + 'transfere')) {
-          if (!args[0]) {
-            message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
-         return;
-           }
-        // We should also make sure that args[0] is a number
-        if (isNaN(args[0])) {
-            message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
-            return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
-             }
-            let defineduser = '';
-            let firstMentioned = message.mentions.users.first();
-            defineduser = (firstMentioned)
-            if (!defineduser) return message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
-            var mentionned = message.mentions.users.first();
-if (!profile[sender.id]) profile[sender.id] = {}
-if (!profile[sender.id].credits) profile[sender.id].credits = 200;
-fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
-if (err) console.error(err);
-})
-      var mando = message.mentions.users.id;
-      if  (!profile[defineduser.id]) profile[defineduser.id] = {}
-      if (!profile[defineduser.id].credits) profile[defineduser.id].credits = 200;
-      profile[defineduser.id].credits += (+args[0]);
-      profile[sender.id].credits += (-args[0]);
-      let mariam = message.author.username
-message.channel.send(`**:moneybag: | ${message.author.username}, has transferrerd ` + "`" + args[0] + "$` to " + `<@${defineduser.id}>**`)
-}
- 
-      });
- 
-      client.on('message', message => {
-          if(!profile[message.author.id]) profile[message.author.id] ={
-              points: 0,
-              level: 1
-          };
-          if(message.author.bot) return;
-          profile[message.author.id].points = Math.floor(profile[message.author.id].points+1);
-          if(profile[message.author.id].points > 100) {
-              profile[message.author.id].points = 0
-              profile[message.author.id].level = Math.floor(profile[message.author.id].level+1);
-              message.channel.send(`**${message.author.username}, You leveld up to __${profile[message.author.id].level}__**`)
-          }
-          fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
-if (err) console.error(err);
-})
-      })//Êã ÇÎÐ ßæÏ ãä ÞÈá Dramex ÕÇÍÈ Probot ÚÔÇä íÔÊÛá ßæÏ áÇÒã ÊÍØ ãáÝ profile.json
 
-client.on('message', message => {
-  let args = message.content.split(" ")
-  if (args[0].toLowerCase().startsWith(prefix+'roles')) {
-    let str = "";
-    var role = message.guild.roles.forEach(role => {
-      str +=" "+role.name+'\n'
+        backwards.on('collect', r => {
+            if (page === 1) return;
+            page--;
+            embed.setDescription(pages[page-1]);
+            embed.setFooter(`Page ${page} of ${pages.length}`);
+            msg.edit(embed)
+        })
+        forwards.on('collect', r => {
+            if (page === pages.length) return;
+      
+      page++;
+            embed.setDescription(pages[page-1]);
+            embed.setFooter(`Page ${page} of ${pages.length}`);
+            msg.edit(embed)
+        })
+        })
     })
-    message.channel.send(`\`\`\`${str}\`\`\``)
-  }
-})
-
-client.on('message', message => {
-     if (message.content === "-servers") {
-     let embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .addField("**| السيرفرات |**" , client.guilds.size)
-  message.channel.sendEmbed(embed);
     }
-});
-
-client.on('message' , async (message) => {
-var prefix = "-"
-    if(message.content.startsWith(prefix + "topinv")) {
-if(message.author.bot) return;
-if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
-  var invites = await message.guild.fetchInvites();
-    invites = invites.array();
-    arraySort(invites, 'uses', { reverse: true });
-    let possibleInvites = ['User Invited |  Uses '];
-    invites.forEach(i => {
-        if (i.uses === 0) { 
-            return;
-        }
-      possibleInvites.push(['\n\ ' +'<@'+ i.inviter.id +'>' + '  :  ' +   i.uses]);
-     //معلومه بسيطه يمديك تكرر العمليهه أكثر من مره
-    })
-    const embed = new Discord.RichEmbed()
- .setColor('RANDOM')
-    .addField("Top Invites." ,`${(possibleInvites)}`)
-
-    message.channel.send(embed)
-    }
-});
-
-client.on('message',async message => {
-     
-     const arraySort = require('array-sort');
-const table = require('table');
-if (message.content.toLowerCase().startsWith(prefix + 'banlist')){
-     let ban = await message.guild.fetchBans().catch(error => {
-        return message.channel.send('Sorry, I don\'t have the proper permissions to view bans!');
-    });
-
-    ban = ban.array();
-    let users = message.guild.fetchBans().id;
-    message.channel.send(`**${message.guild.name} Bans List :**`)
-    ban.forEach(function(ban) {
-         message.channel.send(`${ban} (${ban.id})`)
-    })
-}
- });
-
-client.on('message' , message => {
-  if(message.author.bot) return;
-  if(message.content.startsWith(prefix + "ping")) {
- message.channel.send('pong').then((msg) => {
-var PinG = `${Date.now() - msg.createdTimestamp}`
-var ApL = `${Math.round(client.ping)}`
-      msg.edit(`\`\`\`javascript\nTime taken: ${PinG} ms.\nDiscord API: ${ApL} ms.\`\`\``);
- })
-  }  
- });
-
-function welcomeMessage(tag) {
-
-var welcomeMessages = [`Welcome, ${tag}. We hope you brought pizza.`, `Welcome ${tag}. Leave your weapons by the door`,
-
-`Big ${tag} showed up!`, `${tag} hopped into the server. Kangaroo!!`, `${tag} just showed up. Hold my beer`, `Hey! Listen! ${tag}. has joined!`,
-
-`Swoooosh. ${tag} just landed`, `Ermagherd. ${tag} is here.`, 
-
-`${tag} is here , i can see it!`, `${tag} has joined the server! It's super effective!`,`Welcome, ${tag} We were expecting you ( ͡° ͜ʖ ͡°)`]
-
-return welcomeMessages[Math.floor(Math.random()*welcomeMessages.length)]
-
-}
-
-
-
-client.on("guildMemberAdd", member => {
-
-   var welc = new Discord.RichEmbed()
-
-  .setColor('RANDOM')
-
-  .addField("**⇢**", welcomeMessage(member), true)
-
-   var welcome_channel = member.guild.channels.find("name", "_welcome_");
-
-   welcome_channel.send(welc)
-
-});
+}); 
