0f65655fad31102df44bcbac68141be6440c0f0a
ZALABIA <41113180+iiUMeex@users.noreply.github.com>
2019-02-19 18:23:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,25 +72,6 @@ client.on(`ready`, () => {
     });
 });
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const fs = require("fs");
-const ownerid = ('295216776428388362')
-const package = ('package.json');
-let prefix = "#"
-
-
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
 client.on('message', message => {//help
      if (message.content === "#help") {
   message.channel.send('**تم الارسال لك في الخاص | :ballot_box_with_check:**')
